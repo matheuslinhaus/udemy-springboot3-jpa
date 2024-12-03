3f7d7655b2b555833cd7e1c27a0401d579eb3d46
Matheus R Linhaus <matheuslinhauss@hotmail.com>
2024-12-03 14:43:25 -0300

    Criado um delete para usuarios

--- src/main/java/com/linhaus/course/resources/UserResource.java
+++ src/main/java/com/linhaus/course/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,5 +43,12 @@ public class UserResource {
 		//Retornar o status 201 (creatd) por isso é necessário mortar o Uri.
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		//Retorna um 204
+		return ResponseEntity.noContent().build();
 	}
 }
