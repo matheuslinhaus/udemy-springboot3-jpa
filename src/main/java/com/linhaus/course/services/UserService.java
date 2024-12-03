8602870bdbaa1dd55b9a8547927b761eb1324689
Matheus R Linhaus <matheuslinhauss@hotmail.com>
2024-12-03 17:29:24 -0300

    Criar exceção para delete, usando de situações diferentes

--- src/main/java/com/linhaus/course/services/UserService.java
+++ src/main/java/com/linhaus/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.linhaus.course.entities.User;
 import com.linhaus.course.repositories.UserRepository;
+import com.linhaus.course.services.exceptions.DatabaseException;
 import com.linhaus.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,14 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		if (!repository.existsById(id)) {
+			throw new ResourceNotFoundException(id);
+		}
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	public User update(Long id, User obj) {
